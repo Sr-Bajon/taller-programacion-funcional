baeca6ca304b84f03488cf3ef26c5888dc9ae2c6
Ismael R <ismael.rodriguez@atmira.com>
2017-01-04 11:02:54 +0100

    Ejemplo factorial recursivo expandido

--- taller.js
+++ taller.js
@@ -48,4 +48,37 @@ function factorialVelez(n) {
     factorialVelez(n - 1) * n; // caso contrario,
 }
 
-console.log(factorialVelez(5));
+xconsole.log(factorialVelez(5));
+
+function factorialVelezOneLine(n) {
+  return n === 0 ? 1 : factorialVelezOneLine(n - 1) * n;
+}
+
+function factorialVelezExplicada(n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    var functionResult = factorialVelez(n - 1);
+    var result = functionResult * n;
+    return result;
+  }
+}
+/*
+  - La recursividad se va llamando a si misma sin llegar al return de la función
+  - Solo se devuelve el return correspondiente a la primera llamada a la función,
+    los demas, al ser llamados recursivamente, se resuelven internamente.
+  - Si llamamos a la funcion con:
+    * por ejemplo "5"
+      * La primera vez se llama a si misma con "4"
+        *   Se llama a si misma con 3
+          *   Se llama a si misma con 2
+            *   Se llama a si misma con 1
+              *   Se llama a si misma con 0
+              *   Se resuelve con 1 y devuelve el resultado
+            *   1 x 1 = se resuelve con 1
+          *   2 x 1 = se resuelve con 2
+        *   3 x 2 = se resuelve con 6
+      *   4 x 6 = se resuelve con 24
+    *   5 x 24 = se resuelve con 120 y al ser la primera llamada se devuelve finalmente
+*/
+console.log(factorialVelezExplicada(5));
